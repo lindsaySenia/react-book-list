5ad2070be00afbfb567f4a1213c2d9b535434afa
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2023-05-02 23:07:16 -0400

    update

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,6 +1,10 @@
 function BookShow({ book }) {
 
-    return <div className="book-show">{ book.title }</div>;
+    return (
+        <div className="book-show">
+            { book.title }
+        </div>
+    );
 }
 
 export default BookShow;
