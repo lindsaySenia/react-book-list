9f7ebc4104d17d58f4809a8f8eb2aca011bc162f
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2023-05-03 20:39:42 -0400

    BookEdit set up

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+
 function BookEdit() {
-    return <div>BookEdit</div>;
-}
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log('New title:', title);
+    };
+
+    return (
+            <form className="book-edit" onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input className="input" value={title} onChange={handleChange} />
+                <button className="button is-primary">
+                    Save
+                </button>
+            </form>
+    );
+};
 
 export default BookEdit;
